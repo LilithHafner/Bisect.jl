e06aefc8602df7cb737e3505219cc72d0bc6f43e
Documenter.jl <documenter@juliadocs.github.io>
2023-11-28 17:07:05 +0000

    build based on 342aa24

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Bisect","category":"page"},{"location":"#Bisect","page":"Home","title":"Bisect","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bisect.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Bisect]","category":"page"}]
+}
