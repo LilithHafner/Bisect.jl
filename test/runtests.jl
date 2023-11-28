5da09c5e603fcbe72adf6ae2c8e3dbbfd07058c5
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-28 10:28:57 -0600

    Tell Aqua to be okay with no compat for extras

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using Aqua
 
 @testset "Bisect.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(Bisect)
+        Aqua.test_all(Bisect, deps_compat=false)
+        Aqua.test_deps_compat(Bisect, check_extras=false)
     end
     # Write your tests here.
 end
