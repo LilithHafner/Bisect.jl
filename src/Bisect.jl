d4cf1a668540438f49d4983950b1ed92d14382a2
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-11-30 13:24:06 -0600

    Update Bisect.jl

--- src/Bisect.jl
+++ src/Bisect.jl
@@ -167,7 +167,7 @@ end
 function workflow2(link=ENV["BISECT_TRIGGER_LINK"])
     comment = get_comment(link)
     md = _workflow(comment)
-    HTTP.post("https://lilithhafner.com/lilithhafnerbot_trigger_2.php", body=link * "," * string(md))
+    HTTP.post("https://lilithhafner.com/lilithhafnerbot/trigger_2.php", body=link * "," * string(md))
 end
 
 end
